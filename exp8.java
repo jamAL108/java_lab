c728af8f6d98c6c01a8d036b374c199a973d9682
JamalMydeen <115083239+jamAL108@users.noreply.github.com>
2022-11-15 22:47:27 +0530

    Update exp8.java

--- exp8.java
+++ exp8.java
@@ -1,48 +1,33 @@
-import java.util.Scanner;
-class circle
-{
-double r;
-double accept()
-{
-Scanner sc= new Scanner(System.in);
-System.out.println("enter the of radius of the sphere");
-r=sc.nextInt();
-return r;
+import java.util.*;
+
+class circle{
+    double r;
+    void input(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of sphere");
+        r=sc.nextInt();
+        System.out.println("The radius is" + r);
+    }
+   }
+class area extends circle{
+    double area;
+      void area(){
+          area = 3.14*r*r;
+          System.out.println("the area of circle is"+area);
+      }
 }
-void display(double value)
-{
-System.out.println("the value is:"+value);
+class volume extends area {
+    double volume;
+    void volume(){
+        volume =(4/3)*3.14*r*r*r;
+        System.out.println("the volume of sphere is"+volume);
+    }
 }
-}
-class area extends circle
-{
-double area;
-double area()
-{
-area = 3.1415*r*r;
-return area;
-}
-}
-class volume extends area
-{
-double volume;
-double volume()
-{
-volume=(4/3)*3.1415*r*r*r;
-return volume;
-}
-}
-class exp8
-{
-public static void main(String args[])
-{
-Scanner sc=new Scanner(System.in);
-volume ob = new volume();
-double radius=ob.accept();
-ob.display(radius);
-double area=ob.area();
-ob.display(area);
-double volume=ob.volume();
-ob.display(volume);
-}
+public class exp8 {
+    public static void main(String[] args) {
+        volume obj = new volume();
+        obj.input();
+        obj.area();
+        obj.volume();
+    }
 }
